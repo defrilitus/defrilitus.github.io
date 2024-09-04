028751be3bd775706e7357fb57f69dad846b8b33
defrilitus <34283333+defrilitus@users.noreply.github.com>
2024-09-04 17:32:34 +0200

    Test 1

---
+++ app-files/data.js
@@ -0,0 +1,557 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entree",
+      "name": "Entrée",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.20768835220929205,
+        "pitch": 0.1734840462608247,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14243568629241565,
+          "pitch": 0.5004162051659566,
+          "rotation": 0,
+          "target": "1-sejour"
+        },
+        {
+          "yaw": 0.18416808382011318,
+          "pitch": 0.22261553099489007,
+          "rotation": 1.5707963267948966,
+          "target": "6-degagement"
+        },
+        {
+          "yaw": -0.39403889500814415,
+          "pitch": 0.2577654720337943,
+          "rotation": 5.497787143782138,
+          "target": "4-mezzanine"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6177008134427417,
+          "pitch": -0.20557229525392628,
+          "title": "Porte d'entrée",
+          "text": "Appartement nº 8<br>"
+        },
+        {
+          "yaw": 2.194113219992225,
+          "pitch": 0.8455700677905575,
+          "title": "Palier<br>",
+          "text": "Deuxième étage"
+        },
+        {
+          "yaw": -2.5829631401982596,
+          "pitch": -0.0854118848007257,
+          "title": "Nos voisins",
+          "text": "Appartement nº 7<br>"
+        },
+        {
+          "yaw": -1.0534157900328402,
+          "pitch": 0.13189503819404358,
+          "title": "WC",
+          "text": "Porte WC"
+        },
+        {
+          "yaw": 2.167511322329271,
+          "pitch": 0.23360127047601864,
+          "title": "Escalier",
+          "text": "Escalier vers le premier étage"
+        }
+      ]
+    },
+    {
+      "id": "1-sejour",
+      "name": "Séjour",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.681320386952283,
+        "pitch": -0.0032287694281425416,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46400302474030397,
+          "pitch": 0.16648432836765537,
+          "rotation": 0,
+          "target": "3-salle-a-manger"
+        },
+        {
+          "yaw": -1.0870727970091316,
+          "pitch": 0.1363822947711153,
+          "rotation": 4.71238898038469,
+          "target": "2-cuisine"
+        },
+        {
+          "yaw": -1.171324293709194,
+          "pitch": -0.42522121565104776,
+          "rotation": 0,
+          "target": "4-mezzanine"
+        },
+        {
+          "yaw": -2.319221537794437,
+          "pitch": -0.2330697113935578,
+          "rotation": 0.7853981633974483,
+          "target": "4-mezzanine"
+        },
+        {
+          "yaw": -2.7706342537668522,
+          "pitch": 0.49700534207777025,
+          "rotation": 0,
+          "target": "0-entree"
+        },
+        {
+          "yaw": -2.7790541994614166,
+          "pitch": 0.098597897937303,
+          "rotation": 4.71238898038469,
+          "target": "6-degagement"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cuisine",
+      "name": "Cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.16136074715430837,
+        "pitch": 0.06618977327704911,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.112658148097334,
+          "pitch": 0.5779067125959276,
+          "rotation": 0,
+          "target": "1-sejour"
+        },
+        {
+          "yaw": -1.577780359062153,
+          "pitch": -0.43851280072612653,
+          "rotation": 0,
+          "target": "4-mezzanine"
+        },
+        {
+          "yaw": -1.8120692252715127,
+          "pitch": 0.0035272058090498604,
+          "rotation": 1.5707963267948966,
+          "target": "0-entree"
+        },
+        {
+          "yaw": 2.753566593080354,
+          "pitch": 0.06222558376716947,
+          "rotation": 4.71238898038469,
+          "target": "3-salle-a-manger"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salle-a-manger",
+      "name": "Salle à manger",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6767272151063466,
+        "pitch": 0.0032287694281514234,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4673780738305524,
+          "pitch": -0.49661892832206966,
+          "rotation": 0,
+          "target": "5-mezzanine-suite"
+        },
+        {
+          "yaw": -2.403482827288899,
+          "pitch": 0.6094969040812046,
+          "rotation": 0,
+          "target": "1-sejour"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mezzanine",
+      "name": "Mezzanine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2563401304094803,
+        "pitch": 0.2417982653164934,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5017909465813695,
+          "pitch": 0.6667550386839185,
+          "rotation": 9.42477796076938,
+          "target": "1-sejour"
+        },
+        {
+          "yaw": 0.7387981547788449,
+          "pitch": 0.6036524957122378,
+          "rotation": 0,
+          "target": "5-mezzanine-suite"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9952081970711468,
+          "pitch": 0.387470300420647,
+          "title": "Dressing",
+          "text": "Vestiaire et penderie<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-mezzanine-suite",
+      "name": "Mezzanine suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5547377382043202,
+        "pitch": 0.3196481733868417,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5087732281099608,
+          "pitch": 0.6213381660361819,
+          "rotation": 3.141592653589793,
+          "target": "3-salle-a-manger"
+        },
+        {
+          "yaw": 2.696670762892567,
+          "pitch": 0.8486142953562705,
+          "rotation": 0,
+          "target": "4-mezzanine"
+        },
+        {
+          "yaw": 2.396335932405915,
+          "pitch": 0.30824627642398994,
+          "rotation": 9.42477796076938,
+          "target": "1-sejour"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.631114553320705,
+          "pitch": 0.31835563037723347,
+          "title": "Dressing<br>",
+          "text": "Vestiaire et penderie"
+        }
+      ]
+    },
+    {
+      "id": "6-degagement",
+      "name": "Dégagement",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.19969565926585986,
+        "pitch": 0.03390207899556685,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38035502873784566,
+          "pitch": 0.16900747382381276,
+          "rotation": 6.283185307179586,
+          "target": "7-salle-de-bains"
+        },
+        {
+          "yaw": -0.7116846175672897,
+          "pitch": 0.45169785611610536,
+          "rotation": 0,
+          "target": "8-chambre-principale"
+        },
+        {
+          "yaw": -2.471741198615023,
+          "pitch": 0.650482396307126,
+          "rotation": 0,
+          "target": "0-entree"
+        },
+        {
+          "yaw": -2.2969715415434173,
+          "pitch": 0.15579505322827103,
+          "rotation": 1.5707963267948966,
+          "target": "1-sejour"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4389300922536385,
+          "pitch": 0.050326212124410574,
+          "title": "WC",
+          "text": "Porte WC<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-salle-de-bains",
+      "name": "Salle de bains",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8136454189469884,
+          "pitch": 0.2883169427007033,
+          "rotation": 0,
+          "target": "6-degagement"
+        },
+        {
+          "yaw": -1.4040438220657094,
+          "pitch": 0.49291183582796805,
+          "rotation": 0,
+          "target": "8-chambre-principale"
+        },
+        {
+          "yaw": -2.7853115844718026,
+          "pitch": 0.3627906578129352,
+          "rotation": 0,
+          "target": "0-entree"
+        },
+        {
+          "yaw": -0.9879466440804592,
+          "pitch": 0.5121278712431732,
+          "rotation": 1.5707963267948966,
+          "target": "7-salle-de-bains"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.019452310477872,
+          "pitch": 0.3184048375997328,
+          "title": "Placard à mur<br>",
+          "text": "Placard à mur et chauffe-eau<br>"
+        }
+      ]
+    },
+    {
+      "id": "8-chambre-principale",
+      "name": "Chambre principale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.918397919216023,
+          "pitch": 0.8420421841983092,
+          "rotation": 0,
+          "target": "6-degagement"
+        },
+        {
+          "yaw": 3.1101411359879174,
+          "pitch": 0.10700372962541849,
+          "rotation": 4.71238898038469,
+          "target": "7-salle-de-bains"
+        },
+        {
+          "yaw": -2.8481945633998773,
+          "pitch": 0.12049723300718718,
+          "rotation": 1.5707963267948966,
+          "target": "0-entree"
+        },
+        {
+          "yaw": -1.8333276358423074,
+          "pitch": 0.20252952930082024,
+          "rotation": 4.71238898038469,
+          "target": "6-degagement"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6850672440429744,
+          "pitch": 0.22871410161399908,
+          "title": "Placard à mur<br>",
+          "text": "Placard à mur et hauffe-eau"
+        }
+      ]
+    }
+  ],
+  "name": "Appartement",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
